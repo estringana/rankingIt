b48a4c92ad180dc02ed702d3845bc90a65d3a814
Alejandro Estringana Ruiz <estringana@gmail.com>
2017-02-05 19:03:33 +1100

    Add endpoint to add institutions

---
+++ app/Http/Controllers/InstitutionController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Institution;
+
+class InstitutionController extends Controller
+{
+    public function create(Request $request)
+    {
+        $this->validate($request,
+            ['name' => 'required']
+        );
+
+        $institution = new Institution();
+        $institution->name = $request->get('name');
+        $institution->save();
+
+        return response('', 201);
+    }
+}
